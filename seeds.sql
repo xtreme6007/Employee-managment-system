0d9fd35b33f955d76a3da7ab35ebb4efdfc100c7
xtreme6007 <prestonnichols96@gmail.com>
2020-09-22 08:52:29 -0500

    finished seeds

--- seeds.sql
+++ seeds.sql
@@ -12,3 +12,15 @@ VALUES ("Intern", 40000.00, 1, 25);
 
 INSERT INTO employee (title, salary, department_id, manager_id)
 VALUES ("Manager", 99000.00, 1, null);
+
+
+
+INSERT INTO role (first_name, last_name, role_id)
+VALUES ("John", "Dee", 3);
+
+INSERT INTO role (first_name, last_name, role_id)
+VALUES ("Sarah", "Hopkins", 1);
+
+INSERT INTO role (first_name, last_name, role_id)
+VALUES ("Ron", "Nugget", 2);
+
